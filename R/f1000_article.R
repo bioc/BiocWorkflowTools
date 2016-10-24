4a1dedb0e8f0b546da6cb2bf9585d77f193b7b8a
Andrzej Oleś <andrzej.oles@gmail.com>
2016-10-24 16:39:21 +0000

    Add 'f1000_article' R markdown format function and R markdown 'f1000_article' document template
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocWorkflowTools@122986 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/f1000_article.R
@@ -0,0 +1,58 @@
+#' F1000 article format
+#' 
+#' Format for creating F1000 software articles.
+#'
+#' Creates a latex file that can be uploaded to F1000 Overleaf
+#' 
+#' @inheritParams rmarkdown::pdf_document
+#' @param toc TRUE to include a table of contents in the output
+#' @param number_sections TRUE to number section headings
+#' @param keep_tex Keep the intermediate tex file used in the conversion to PDF
+#' @param ... Arguments to \code{rmarkdown::pdf_document}
+#' 
+#' @return R Markdown output format to pass to
+#'   \code{\link[rmarkdown:render]{render}}
+#'
+#' @examples
+#'
+#' \dontrun{
+#' rmarkdown::draft("MyArticle.Rmd", template="f1000_article", package="BiocWorkflowTools")
+#' }
+#'
+#' @importFrom bookdown pdf_book
+#' 
+#' @export
+f1000_article <- function(toc = FALSE,
+                          number_sections = FALSE,
+                          keep_tex = TRUE,
+                          ...) {
+  
+  base_format_postprocessed <- function(toc,
+                                        number_sections,
+                                        keep_tex,
+                                        ...) {
+    template <- system.file('rmarkdown', 'templates', 'f1000_article', "resources", "template.tex",
+                            package = "BiocWorkflowTools")
+    
+    config <- rmarkdown::pdf_document(toc = toc,
+                                      number_sections = number_sections,
+                                      keep_tex = keep_tex,
+                                      template = template,
+                                      ...)
+    
+    config$post_processor <- function(metadata, input, output, clean, verbose) {
+      .processPandocTables(output, output)
+      output
+    }
+    
+    config
+  }
+  
+  # use bookdown::pdf_book because of the added capability of cross-referencing
+  pdf_book(toc = toc,
+           number_sections = number_sections,
+           keep_tex = keep_tex,
+           ...,
+           base_format = base_format_postprocessed)
+  
+}
