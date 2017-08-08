3ff4a640076015472e773f594796e79e264ba1e7
Andrzej Oleś <andrzej.oles@gmail.com>
2017-08-08 12:37:50 +0000

    - Sanitize code of `f1000_article` - Set default figure dimensions to actual paragraph width - Add argument specifying default figure alignment
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocWorkflowTools@131808 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/f1000_article.R
+++ R/f1000_article.R
@@ -5,6 +5,7 @@
 #' Creates a latex file that can be uploaded to F1000 Overleaf
 #' 
 #' @inheritParams rmarkdown::pdf_document
+#' @param fig_align Default alignment of figures. Possible values are "center" (default) "left" and "right.
 #' @param ... Arguments to \code{rmarkdown::pdf_document}
 #' 
 #' @return R Markdown output format to pass to
@@ -17,136 +18,131 @@
 #' }
 #'
 #' @importFrom bookdown pdf_book
-#' @importFrom rmarkdown includes_to_pandoc_args includes
+#' @importFrom rmarkdown output_format
+#' @importFrom tools file_path_sans_ext
 #' 
 #' @export
 f1000_article <- function(toc = FALSE,
                           number_sections = FALSE,
+                          fig_width = 5.67,
+                          fig_height = fig_width,
+                          fig_align = "center",
                           keep_tex = TRUE,
                           md_extensions = "+link_attributes",
                           pandoc_args = "--wrap=preserve",
-                          extra_dependencies = NULL,
                           ...) {
   
-  base_format_postprocessed <- function(toc,
-                                        number_sections,
-                                        keep_tex,
-                                        md_extensions,
-                                        pandoc_args,
-                                        extra_dependencies,
-                                        ...) {
+  template_dir <-  system.file(package = 'BiocWorkflowTools', 
+                               'rmarkdown', 'templates', 'f1000_article')
+  template <- file.path(template_dir, 'resources', 'template.tex')
+  template_files <- file.path(template_dir, 'skeleton', c('f1000_styles.sty', 'F1000header.png'))
+  
+  ## preprocessor setting pandoc variables
+  pre_processor <- function(metadata, input_file, runtime, knit_meta, files_dir, output_dir) {
+    args <- c()
     
-    template <- system.file("rmarkdown", "templates", "f1000_article", "resources", "template.tex",
-                            package = "BiocWorkflowTools")
+    defaults <- list(
+      documentclass = "extarticle",
+      fontsize = "9pt",
+      papersize = "a4",
+      citationoptions = "numbers",
+      letcitecitep = if (isTRUE(grepl("round", metadata$citationoptions, fixed=TRUE))) "yes" else "no"
+    )
     
-    template_files <- system.file(package = "BiocWorkflowTools", 
-                                  'rmarkdown', 'templates', 'f1000_article', 'skeleton',
-                                  c('f1000_styles.sty', 'F1000header.png'))
+    ## set to default if not specified in document header
+    vars <- defaults[!names(defaults) %in% names(metadata)]
     
-    config <- rmarkdown::pdf_document(toc = toc,
-                                      number_sections = number_sections,
-                                      template = template,
-                                      keep_tex = keep_tex,
-                                      md_extensions = md_extensions,
-                                      pandoc_args = pandoc_args,
-                                      extra_dependencies = extra_dependencies,
-                                      pandoc_args = 
-                                      ...)
+    ## remove disabled boolean options
+    vars <- vars[!tolower(vars) %in% c("no", "false")]
     
-    intermediates <- config$intermediates_generator
-    
-    # run when document is rendered to a different directory than the input file
-    config$intermediates_generator <- function(original_input, encoding, intermediates_dir) {
-      ## run the default intermediates generator
-      intermediates <- if (is.function(intermediates)) intermediates(original_input, encoding, intermediates_dir)
-
-      ## copy f1000 style file & header
-      file.copy(template_files, normalizePath(intermediates_dir))
-
-      ## append style template files
-      intermediates <- c(intermediates, basename(template_files))
-      
-      intermediates
-    }
-
-    ## preprocessor to set pandoc variables
-    config$pre_processor <- function(metadata, input_file, runtime, knit_meta, files_dir, output_dir) {
-      args <- c()
-      
-      defaults <- list(
-        documentclass = "extarticle",
-        fontsize = "9pt",
-        papersize = "a4",
-        citationoptions = "numbers",
-        letcitecitep = if (isTRUE(grepl("round", metadata$citationoptions, fixed=TRUE))) "yes" else "no"
-      )
-      
-      ## set to default if not specified in document header
-      vars <- defaults[!names(defaults) %in% names(metadata)]
-      
-      ## remove disabled boolean options
-      vars <- vars[!tolower(vars) %in% c("no", "false")]
-      
-      ## format as pandoc command-line args
-      if (length(vars) > 0L) {
-        vars <- paste(names(vars), vars, sep="=")
-        vars <- c(rbind(rep("--variable", length(vars)), vars))
-        args <- c(args, vars)
-      }
-      
-      ## following code copied from 'pdf_pre_processor' defined in 'rmarkdown::pdf_document'
-      format_deps <- list()
-      format_deps <- append(format_deps, extra_dependencies)
-      
-      if (rmarkdown:::has_latex_dependencies(knit_meta)) {
-        all_dependencies <- if (is.null(format_deps)) list() else format_deps
-        all_dependencies <- append(all_dependencies, rmarkdown:::flatten_latex_dependencies(knit_meta))
-        filename <- tempfile()
-        rmarkdown:::latex_dependencies_as_text_file(all_dependencies, filename)
-        args <- c(args, includes_to_pandoc_args(includes(in_header = filename)))
-      }
-      
-      args
+    ## format as pandoc command-line args
+    if (length(vars) > 0L) {
+      vars <- paste(names(vars), vars, sep="=")
+      vars <- c(rbind(rep("--variable", length(vars)), vars))
+      args <- c(args, vars)
     }
     
-    config$post_processor <- function(metadata, input, output, clean, verbose) {
-      lines <- readUTF8(output)
-      
-      ## insert author affiliations
-      lines <- BiocStyle:::modifyLines(lines,
-                                       from = '%% AUTH AFFIL %%',
-                                       insert = BiocStyle:::auth_affil_latex(metadata))
-      
-      ## convert instances of \longtable to \table
-      lines <- .processPandocTables(lines)
-      
-      ## remove calls to pandoc's \tightlist macro
-      lines <- lines[!grepl("^[[:blank:]]*\\\\tightlist$", lines)]
-      
-      writeUTF8(lines, output)
-      
-      output
-    }
-    
-    
-    # use `pre_knit` to copy f1000 template files as this seems to be the only 
-    # function which is aware of the original input file name passed to `render` 
-    config$pre_knit <- function(input, ...) {
-      # copy f1000 style file & header
-      file.copy(template_files, dirname(normalizePath(input)))
-    }
-    
-    config
+    args
   }
   
-  # use bookdown::pdf_book because of the added capability of cross-referencing
-  pdf_book(toc = toc,
-           number_sections = number_sections,
-           keep_tex = keep_tex,
-           md_extensions = md_extensions,
-           pandoc_args = pandoc_args,
-           extra_dependencies = extra_dependencies,
-           ...,
-           base_format = base_format_postprocessed)
+  post_processor <- function(metadata, input, output, clean, verbose) {
+    lines <- readUTF8(output)
+    
+    ## insert author affiliations
+    lines <- BiocStyle:::modifyLines(lines,
+                                     from = '%% AUTH AFFIL %%',
+                                     insert = BiocStyle:::auth_affil_latex(metadata))
+    
+    ## convert instances of \longtable to \table
+    lines <- .processPandocTables(lines)
+    
+    ## remove calls to pandoc's \tightlist macro
+    lines <- lines[!grepl("^[[:blank:]]*\\\\tightlist$", lines)]
+    
+    writeUTF8(lines, output)
+    
+    output
+  }
   
+  # use `pre_knit` to copy f1000 template files as this seems to be the only 
+  # function which is aware of the original input file name passed to `render` 
+  pre_knit <- function(input, ...) {
+    # copy f1000 style file & header
+    file.copy(template_files, dirname(normalizePath(input)))
+  }
+  
+  ## add a dummy geometry field to document front matter in order to prevent the
+  ## default pre processor 'pdf_pre_processor' defined in 
+  ## 'rmarkdown::pdf_document' from setting the 'geometry' variable to pandoc
+  post_knit <- function(metadata, input_file, runtime, ...) {
+    if ( is.null(metadata$geometry) ) {
+      output_file <- sprintf("%s.knit.md", file_path_sans_ext(input_file))
+      lines <- readUTF8(output_file)
+      lines <- rmarkdown:::partition_yaml_front_matter(lines)
+      front_matter <- lines$front_matter
+      
+      ## append geometry entry to yaml front matter
+      l <- length(front_matter)
+      front_matter <- c(front_matter[-l], "geometry: default", front_matter[l])
+      
+      writeUTF8(c(front_matter, lines$body), output_file)
+    }
+  }
+  
+  # run when document is rendered to a different directory than the input file
+  intermediates_generator <- function(original_input, encoding, intermediates_dir) {
+    ## copy f1000 style file & header
+    file.copy(template_files, normalizePath(intermediates_dir))
+    
+    ## append style template files
+    basename(template_files)
+  }
+  
+  knitr <- list(opts_chunk = list(fig.align = fig_align))
+  
+  # use bookdown::pdf_book because of the added capability of cross-referencing
+  config <- output_format(knitr = knitr, pandoc = NULL,
+                          clean_supporting = FALSE,
+                          pre_knit = pre_knit,
+                          post_knit = post_knit,
+                          pre_processor = pre_processor,
+                          intermediates_generator = intermediates_generator,
+                          post_processor = post_processor,
+                          base_format = pdf_book(toc = toc,
+                                                 number_sections = number_sections,
+                                                 fig_width = fig_width,
+                                                 fig_height = fig_height,
+                                                 template = "default",
+                                                 keep_tex = keep_tex,
+                                                 md_extensions = md_extensions,
+                                                 pandoc_args = pandoc_args,
+                                                 base_format = rmarkdown::pdf_document,
+                                                 ...)
+  )
+  
+  ## override the default document template which is used in order to retain
+  ## some original template-dependent rmarkdown functionality
+  config$pandoc$args[ match("--template", config$pandoc$args) + 1L ] <- template
+  
+  config
 }
